24a64eb9845c0bc7efde2971a7188050721899df
HubiRto <hubert.rybicki.hr@gmail.com>
2022-05-31 21:29:23 +0200

    Pierwsze pliki

---
+++ src/main/java/pl/pomoku/babymode/BabyMode.java
@@ -0,0 +1,12 @@
+package pl.pomoku.babymode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BabyMode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
